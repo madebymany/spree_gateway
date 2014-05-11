3bce7570798c32090effa2a8062dd8c483dc8b25
Chris Bell <cjbell1988@gmail.com>
2014-05-11 01:22:00 +0100

    Add an update payment method. Slightly refactor the store with options hash.

--- app/models/spree/gateway/spreedly_gateway.rb
+++ app/models/spree/gateway/spreedly_gateway.rb
@@ -13,23 +13,25 @@ module Spree
     end
 
     def create_profile(payment)
-      if payment.source.gateway_payment_profile_id.nil?
-        options = {}
-        options[:email] = payment.order.email
-        options.merge!({ :billing_address  => payment.order.bill_address.try(:active_merchant_hash),
-                        :shipping_address => payment.order.ship_address.try(:active_merchant_hash) })
+      return unless payment.source.gateway_payment_profile_id.nil?
 
-        response = provider.store(payment.source, options)
+      # Reload the order so we actually get the addresses
+      payment.order.reload
 
-        if response.success?
-          #or should this be setting the  gateway_payment_profile_id
-          last_4 = response.params['payment_method_last_four_digits']
-          payment.source.last_digits = last_4 if last_4
-          payment.source.gateway_customer_profile_id = response.params['payment_method_token']
-          payment.source.save!
-        else
-          payment.send(:gateway_error, response.message)
-        end
+      options = {
+        email: payment.order.email
+      }.merge! addresses_for(payment)
+
+      response = provider.store(payment.source, options)
+
+      if response.success?
+        #or should this be setting the  gateway_payment_profile_id
+        last_4 = response.params['payment_method_last_four_digits']
+        payment.source.last_digits = last_4 if last_4
+        payment.source.gateway_customer_profile_id = response.params['payment_method_token']
+        payment.source.save!
+      else
+        payment.send(:gateway_error, response.message)
       end
     end
 
@@ -46,6 +48,18 @@ module Spree
     def void(identifier, creditcard, options = {})
       identifier = identifier || creditcard.gateway_customer_profile_id
       provider.void(identifier, options)
+    end
+
+    def update(identifier, creditcard, options = {})
+      identifier = creditcard.gateway_customer_profile_id || identifier
+      provider.update(identifier, options)
+    end
+
+  private
+
+    def addresses_for(payment)
+      { billing_address:  payment.order.bill_address.try(:active_merchant_hash),
+        shipping_address: payment.order.ship_address.try(:active_merchant_hash) }
     end
 
   end
